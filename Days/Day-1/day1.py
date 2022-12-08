3fe2632daca4ed45127ad29bc81d4db12a9cd5cf
Vincent-devSG <vincent.pompei@edu.ece.fr>
2022-12-08 18:42:31 +0100

    feat: add part2

--- Days/Day-1/day1.py
+++ Days/Day-1/day1.py
@@ -23,17 +23,27 @@ def main():
             elv_list.append(Elv(elv_food))
             elv_food = []
 
-    """""
-    for i in range(len(elv_list)):
-        print("i : ", i, "      sum: ", elv_list[i].get_sum(), "     list of food: ", elv_list[i].food)
-    """
 
-    max = 0
-    for i in range(len(elv_list)):
-        if elv_list[i].get_sum()>max:
-            max = elv_list[i].get_sum()
+    # for i in range(len(elv_list)):
+    #     print("i : ", i, "      sum: ", elv_list[i].get_sum(), "     list of food: ", elv_list[i].food)
 
-    print(max)
+    # max_food = 0
+    # for i in range(len(elv_list)):
+    #     if elv_list[i].get_sum() > max_food:
+    #         max_food = elv_list[i].get_sum()
+    #
+    # print(max_food)
+
+    elv_sum = []
+    for i in range(len(elv_list)):
+        elv_sum.append(elv_list[i].get_sum())
+
+    tot = 0
+    elv_sum.sort(reverse=True)
+    for i in elv_sum[:3]:
+        tot += i
+
+    print(tot)
 
 if __name__ == "__main__":
     main()
