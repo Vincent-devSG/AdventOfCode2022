54a2b78c4531a23c6c8116aefaefccd3badb6ec1
Vincent-devSG <vincent.pompei@edu.ece.fr>
2022-12-08 15:23:43 +0100

    add: day1

---
+++ Days/Day-1/day1.py
@@ -0,0 +1,8 @@
+
+class Elv:
+    
+    def __init__(self, food):
+        self.food = food
+    
+f = open("C:\Users\Vincent\Documents\AdventOfCode2022\Days\Day-1\List_Elvs.txt", "r")
+print(f.read())
